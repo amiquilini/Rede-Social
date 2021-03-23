a9e5c98850db9d07424bd3b34fb956f6cdbca8cd
Amanda M <60985740+amiquilini@users.noreply.github.com>
2021-03-23 14:09:45 -0300

    Update Rede.java

--- Rede.java
+++ Rede.java
@@ -9,12 +9,12 @@ class Rede {
 
     // Métodos
     public void adicionarAmigo(Amigo a) {
-        amigos.add(a);  // Adiciona o amigo na rede
+        amigos.add(a);  // Adiciona o usuário na rede
     }
 
     public String timeline() {
-        for (int i = 0; i < amigos.size(); i++) {   // iteração na lista "amigos", que armazena todos os usuários da rede
-            timeline = timeline + amigos.get(i).retornaPosts(); // concatena os posts de cada usuário para mostrar em ordem na timeline
+        for (Amigo a: amigos) {   // percorre a lista "amigos", que armazena todos os usuários da rede
+            timeline = timeline + a.retornaPosts(); // concatena os posts de cada usuário para mostrar em ordem na timeline
         }
         return timeline;
     }
