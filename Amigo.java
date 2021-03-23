f8a8d963f22ee5a7d708b340c34f873e6332a93d
Amanda M <60985740+amiquilini@users.noreply.github.com>
2021-03-23 14:10:26 -0300

    Update Amigo.java

--- Amigo.java
+++ Amigo.java
@@ -11,21 +11,22 @@ class Amigo {
     // Métodos
     public void postar(Post p){posts.add(p);} // adiciona o post na lista de posts do usuário
 
-    public void curtir(Post p){p.curtiram.add(this.nome);} // adiciona o nome do usuário na lista de curtidas do post
+    public void curtir(Post p){p.curtidas.add(this.nome);} // adiciona o nome do usuário na lista de curtidas do post
 
     public Post retornaPostMaisCurtido(){
         maiscurtido = posts.get(0);
-        for (int i = 0; i < posts.size(); i++) { // iteração na lista "posts", que armazena todos os post do usuário
-            if (posts.get(i).curtiram.size() > maiscurtido.curtiram.size()){ // compara o número de curtidas em cada post
-                maiscurtido = posts.get(i);
+        for (Post post: posts) { // percorre a lista "posts", que armazena todos os post do usuário
+            if (post.curtidas.size() > maiscurtido.curtidas.size()){ // compara o número de curtidas em cada post
+                maiscurtido = post;
             }
         }
         return maiscurtido;
     }
+
     public String retornaPosts(){
         p = nome + ": \n";
-        for (int j = 0; j < posts.size(); j++) {
-            p = p + posts.get(j) + "\n";
+        for (Post post: posts) { // percorre a lista de posts do usuário
+            p = p + post + "\n";
         }
         return p; // retorna todos os posts desse usuário
     }
